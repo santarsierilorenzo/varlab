d24c9e7869b05155a369a61139e60fa319eac21b
santarsierilorenzo <lorenzo.santarsieri@outlook.com>
2026-02-22 20:02:37 +0000

    root __init__ mod

--- varlab/__init__.py
+++ varlab/__init__.py
@@ -1,3 +1,4 @@
+from . import backtesting
 from .var import var
 from .es import es
 from .window import (
@@ -6,3 +7,13 @@ from .window import (
     expanding_es,
     expanding_var
 )
+
+__all__ = [
+    "var",
+    "es",
+    "rolling_var",
+    "rolling_es",
+    "expanding_var",
+    "expanding_es",
+    "backtesting",
+]
